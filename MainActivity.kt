e6487768ff509f5d0df6459674f9dfea0f95d84c
Temp0ai <emailtempemaol@gmail.com>
2026-05-24 04:25:23 +0530

    Add MainActivity for TeaCoffeeCRM app

---
+++ MainActivity.kt
@@ -0,0 +1,33 @@
+package com.teacoffeecrm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.teacoffeecrm.ui.navigation.AppNavigation
+import com.teacoffeecrm.ui.theme.TeaCoffeeCRMTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            TeaCoffeeCRMTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavigation()
+                }
+            }
+        }
+    }
+}
