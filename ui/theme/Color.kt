70a962e151f945980fb93375511f6f95230ceb25
Temp0ai <emailtempemaol@gmail.com>
2026-05-24 04:26:09 +0530

    Add color definitions for theme in Color.kt

---
+++ ui/theme/Color.kt
@@ -0,0 +1,32 @@
+package com.teacoffeecrm.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Brand Colors
+val TeaBrown = Color(0xFF4E342E)
+val TeaBrownDark = Color(0xFF3E2723)
+val TeaBrownLight = Color(0xFF6D4C41)
+val CoffeeBlack = Color(0xFF1C0A00)
+val GoldAccent = Color(0xFFFFCA28)
+val CreamWhite = Color(0xFFFFF8E1)
+
+// Status Colors
+val LeadNew = Color(0xFF1565C0)
+val LeadContacted = Color(0xFF6A1B9A)
+val LeadInterested = Color(0xFFE65100)
+val LeadNegotiating = Color(0xFFFFCA28)
+val LeadConverted = Color(0xFF2E7D32)
+val LeadLost = Color(0xFF757575)
+
+// Category Colors
+val TeaColor = Color(0xFF8BC34A)
+val CoffeeColor = Color(0xFF795548)
+val NescafeColor = Color(0xFFE53935)
+val MachineColor = Color(0xFF1E88E5)
+
+// UI Colors
+val SurfaceLight = Color(0xFFFFFBFF)
+val SurfaceDark = Color(0xFF1C1B1F)
+val ErrorRed = Color(0xFFBA1A1A)
+val SuccessGreen = Color(0xFF2E7D32)
+val WarningOrange = Color(0xFFE65100)
